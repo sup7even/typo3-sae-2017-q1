413557309a8c53f609cc640fd9e6ad32ad5135cf
Josef Glatz <j.glatz@supseven.at>
2017-02-14 16:18:02 +0100

    [TASK] Initial Commit ext:theme

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Theme',
+    'description' => '',
+    'category' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => '',
+    'author_email' => '',
+    'author_company' => '',
+    'version' => '7.6.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.6.0-7.6.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
